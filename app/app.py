b8327f7d5cf676f0337a45779398452461dad5f9
Fausto C <faustocamp545@gmail.com>
2023-09-17 23:06:32 -0500

    feature: keyboard reading implementation

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 from app.movement import Movement
+import keyboard
 
 
 def tetris():
+    # initial dashboard
     screen = [
         ["🔳", "🔲", "🔲", "🔲", "🔲", "🔲", "🔲", "🔲", "🔲", "🔲"],
         ["🔳", "🔳", "🔳", "🔲", "🔲", "🔲", "🔲", "🔲", "🔲", "🔲"],
@@ -16,25 +18,40 @@ def tetris():
     ]
     print_screen(screen)
 
+    # the rotation will be initialized with 0 value
     rotation = 0
 
-    screen, rotation = move_piece(screen, Movement.ROTATE, rotation)
-    screen, rotation = move_piece(screen, Movement.ROTATE, rotation)
-    screen, rotation = move_piece(screen, Movement.ROTATE, rotation)
-    screen, rotation = move_piece(screen, Movement.ROTATE, rotation)
+    while (True):
+        event = keyboard.read_event()
+        if event.name == "esc":
+            break
+        elif event.event_type == keyboard.KEY_DOWN:  # an other key has been pressed
+            if event.name == "down":
+                movement = Movement.DOWN
+            elif event.name == "right":
+                movement = Movement.RIGHT
+            elif event.name == "left":
+                movement = Movement.LEFT
+            elif event.name == "space":
+                movement = Movement.ROTATE
+            else:
+                continue
+            (screen, rotation) = move_piece(screen, movement, rotation)
 
 
 def print_screen(screen: list):
     print("\nTetris\n")
     for row in screen:
+        # use join to concatenate a list of strings
         print("".join(map(str, row)))
 
 
 def move_piece(screen: list, movement: Movement, rotation: int) -> (list, int):
 
-    new_screen = [["🔲"] * 10 for _ in range(10)]
+    new_screen = [["🔲"] * 10 for _ in range(10)]  # create a 10 x 10 matrix
 
     rotation_item = 0
+    # how many a piece have to move in each rotation
     rotations = [
         [(1, 1), (0, 0), (-2, 0), (-1, -1)],
         [(0, 1), (-1, 0), (0, -1), (1, -2)],
@@ -44,6 +61,7 @@ def move_piece(screen: list, movement: Movement, rotation: int) -> (list, int):
 
     new_rotation = rotation
     if movement is Movement.ROTATE:
+        # increment or reset the rotation for each iteration
         new_rotation = 0 if rotation == 3 else rotation + 1
 
     for row_index, row in enumerate(screen):
@@ -65,13 +83,16 @@ def move_piece(screen: list, movement: Movement, rotation: int) -> (list, int):
                         new_row_index = row_index
                         new_column_index = column_index - 1
                     case Movement.ROTATE:
-                        new_row_index = row_index + rotations[new_rotation][rotation_item][0]
-                        new_column_index = column_index + rotations[new_rotation][rotation_item][1]
+                        new_row_index = row_index + \
+                            rotations[new_rotation][rotation_item][0]
+                        new_column_index = column_index + \
+                            rotations[new_rotation][rotation_item][1]
                         rotation_item += 1
 
                 if new_row_index <= 9 and new_column_index >= 0 and new_column_index <= 9:
                     new_screen[new_row_index][new_column_index] = "🔳"
                 else:
+                    print("📢Movement not allowed")
                     return (screen, rotation)
 
     print_screen(new_screen)
